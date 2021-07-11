ce6b3a5da10f55556eea071f27162f6c0cb871a0
Joao Victor Calvo Fracasso <joao.fracasso@outlook.com>
2021-07-11 12:57:05 -0300

    create folder data

--- app/test/test_predict.py
+++ app/test/test_predict.py
@@ -4,12 +4,16 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
 import onnxruntime as ort
-
 import pytest
 from predict_model import get_prediction
 from src.datasets.lapsbm import Lapsbm
 
+
+if not os.path.isdir("./data"):
+    os.makedirs("./data")
+
 test_dataset = Lapsbm("./data", url="lapsbm-test", download=True)
+
 
 @pytest.mark.parametrize('audio', test_dataset._walker)
 def test_get_prediction(audio):
